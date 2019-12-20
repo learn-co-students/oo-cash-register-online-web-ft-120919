fcde2612bec285db17b1158e9a5af0e7486aff90
nwscala <nwscala@gmail.com>
2019-12-20 17:34:36 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,8 +1,36 @@
 class CashRegister
-    attr_accessor :total, :employee_discount
+    attr_accessor :total, :discount, :cart, :last_transaction
 
-    def initialize(employee_discount = 0)
+    def initialize(discount = 0)
         @total = 0
-        @employee_discount = employee_discount
+        @cart = []
+        @last_transaction = 0
+        @discount = discount
+    end
+
+    def add_item(item_name, item_price, item_quantity = 1)
+        item_quantity.times do 
+            @cart << item_name
+        end
+        @last_transaction = (item_price * item_quantity)
+        @total += @last_transaction
+    end
+
+    def apply_discount
+        if @discount > 0
+            discount_factor = 1.0 - (@discount/100.0)
+            @total *= discount_factor
+            return "After the discount, the total comes to $#{@total.to_i}."
+        else
+            return "There is no discount to apply."
+        end
+    end
+
+    def items
+        @cart
+    end
+
+    def void_last_transaction
+        @total -= @last_transaction
     end
 end
