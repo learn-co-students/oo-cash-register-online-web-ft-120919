38c0eb24faf5295db5e7504d6e7fa55c2842752a
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-23 15:45:54 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
 
+
+
+class CashRegister
+  
+  attr_accessor :total, :discount
+  
+  def initialize(total = 0)
+    
+    @total = total
+    
+    @discount = discount
+  end
+  
+  def self.discount(amount)
+    100.0 % 5 = amount
+  end
+  
+end
